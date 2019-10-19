c642053b2ca763a5e04cd1a79552ae582b7c651c
dmitryderenok <derenok.dmitry@gmail.com>
2019-10-19 17:41:09 +0300

    MIC-1 Add Light entity
    + added properties for mysql
    - deleted tests

--- src/main/kotlin/smart/home/project/model/Light.kt
+++ src/main/kotlin/smart/home/project/model/Light.kt
@@ -7,13 +7,13 @@ import javax.persistence.GenerationType.AUTO
 import javax.persistence.Id
 
 @Entity
-class Light {
+class Light(
     @Id
     @GeneratedValue(strategy = AUTO)
-    private val id: Long? = null
+    private val id: Long? = null,
 
     @Column(nullable = false, unique = true)
-    var name: String? = null
+    var name: String? = null,
 
     var lightStatus: Boolean = false
-}
+)
